c460675f4a7ce2cb07ecd82710e230fc70bf2d11
Jonathan Hickford <jonathan.hickford@gmail.com>
2015-06-30 14:17:01 +0100

    Added new email account

---
+++ funnel_daily/funnel_daily.rb
@@ -0,0 +1,88 @@
+require 'rubygems'
+require 'bundler/setup'
+
+require 'CSV'
+
+require File.join(__dir__, '../common/lighthouse_analytics')
+
+CACHED_DATA_STORE = File.join(__dir__, "datastore.cache")
+OUTPUT_CSV = "#{$0}".ext('.csv')
+
+GA_PROFILE_LIGHTHOUSE_OLD = 'ga:89776902' # Old account id
+GA_PROFILE_LIGHTHOUSE = 'ga:96336725' # New excluding Redgate id
+GA_PROFILE_REDGATE = 'ga:53846' # Redgate - www.redgate.com
+
+
+
+def output_csv(datastore)
+	CSV.open(OUTPUT_CSV, "wb") do |csv|
+		csv <<   ["Date", "Product Pageviews", "Downloads", "New Installs", "New Engaged Teams"]
+		
+		datastore.keys.sort.each do |date_key|
+			csv << [ 
+				date_key,
+				datastore[date_key]['product pageviews'],
+				datastore[date_key]['downloads'],
+				datastore[date_key]['new installs'],
+				datastore[date_key]['engaged new installs']
+			]
+		end
+	end
+
+end
+
+
+
+if __FILE__ == $0
+	client, analytics = setup_google()
+	datastore = setup_datastore()
+
+	start_date_range = Date.new(2015, 1, 11)   # A Sunday
+  end_date_range = Date.today - 1
+
+
+  #puts start_date_range
+  #puts end_date_range
+
+
+	# Itterate over weeks in report range
+  (start_date_range..end_date_range).each do |start_date|
+    end_date = start_date 
+
+		date_key =  "#{start_date}"
+
+		#puts date_key
+
+		datastore[date_key] = Hash.new if !datastore.key?(date_key)
+
+		unless datastore[date_key].key?('new installs')
+			datastore[date_key]['new installs'] = get_new_users(GA_PROFILE_LIGHTHOUSE, client, analytics, start_date, end_date)
+		end
+
+		unless datastore[date_key].key?('engaged new installs')
+			datastore[date_key]['engaged new installs'] = get_engaged_new_users(GA_PROFILE_LIGHTHOUSE, client, analytics, start_date, end_date)
+		end
+
+		# From Redgate account
+		unless datastore[date_key].key?('downloads')
+			datastore[date_key]['downloads'] = get_total_downloads(GA_PROFILE_REDGATE, client, analytics, start_date, end_date)
+		end
+
+		unless datastore[date_key].key?('product pageviews')
+			datastore[date_key]['product pageviews'] = get_total_product_page_views(GA_PROFILE_REDGATE, client, analytics, start_date, end_date)
+		end
+
+		
+		puts date_key +
+			" " + datastore[date_key]['product pageviews'].to_s + 
+      " " + datastore[date_key]['downloads'].to_s +
+      " " + datastore[date_key]['new installs'].to_s + 
+      " " + datastore[date_key]['engaged new installs'].to_s
+	end
+
+	save_datastore(datastore)
+
+	output_csv(datastore)
+end
+
+
---
+++ usage-reports-email-account/usage-email-account.rb
@@ -0,0 +1,78 @@
+require 'rubygems'
+require 'bundler/setup'
+
+require 'CSV'
+
+require File.join(__dir__, '../common/lighthouse_analytics')
+
+CACHED_DATA_STORE = File.join(__dir__, "datastore.cache")
+OUTPUT_CSV = "#{$0}".ext('.csv')
+
+#GA_PROFILE = 'ga:89776902' # Old account id
+#GA_PROFILE = 'ga:96336725' # New excluding Redgate id
+GA_PROFILE = 'ga:103931313' # Email excluding Redgate id
+
+
+
+def output_csv(datastore)
+	CSV.open(OUTPUT_CSV, "wb") do |csv|
+		csv <<   ["Period ending on", "Total teams in last 30 days", "New teams in last 30 days", "Engaged teams in last 30 days", "Retained teams in last 30 days"]
+		
+		datastore.keys.sort.each do |end_date|
+			csv << [ end_date, 
+
+				datastore[end_date]['total in last 30 days'],
+				datastore[end_date]['new in last 30 days'],
+				datastore[end_date]['engaged in last 30 days'],
+				datastore[end_date]['retained in last 30 days']
+
+			]
+		end
+	end
+
+end
+
+
+
+if __FILE__ == $0
+	client, analytics = setup_google()
+	datastore = setup_datastore()
+
+	today = Date.today
+  start = Date.new(2015,6,15)
+
+	(start..today-1).each do |end_date|
+		datastore[end_date] = Hash.new if !datastore.key?(end_date)
+
+	
+		unless datastore[end_date].key?('total in last 30 days')
+			datastore[end_date]['total in last 30 days'] = get_total_users(GA_PROFILE, client, analytics, end_date-30, end_date)
+		end
+
+			
+		unless datastore[end_date].key?('new in last 30 days')
+			datastore[end_date]['new in last 30 days'] = get_new_users(GA_PROFILE, client, analytics, end_date-30, end_date)
+		end
+
+	
+		unless datastore[end_date].key?('engaged in last 30 days')
+			datastore[end_date]['engaged in last 30 days'] = get_engaged_users(GA_PROFILE, client, analytics, end_date-30, end_date)
+		end
+
+		unless datastore[end_date].key?('retained in last 30 days')
+			datastore[end_date]['retained in last 30 days'] = get_retained_users(GA_PROFILE, client, analytics, end_date-30, end_date)
+		end
+
+		puts end_date.to_s +
+			" " + datastore[end_date]['total in last 30 days'].to_s +
+			" " + datastore[end_date]['new in last 30 days'].to_s + 
+			" " + datastore[end_date]['engaged in last 30 days'].to_s +
+			" " + datastore[end_date]['retained in last 30 days'].to_s
+	end
+
+	save_datastore(datastore)
+
+	output_csv(datastore)
+end
+
+
